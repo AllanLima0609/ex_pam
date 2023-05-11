44dd00aebb2dcd55afbd5da0d0650346c629329f
AllanLima0609 <124743524+AllanLima0609@users.noreply.github.com>
2023-05-11 20:47:14 -0300

    11/05

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -1,13 +1,50 @@
+import React, {useState} from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default function apostila03() {
+
+  
+  const [contador, setContador] = useState(0);
+  function AddNumber(){
+    setContador(contador +1);
+   
+  }
+
+  function SubNumber(){
+   setContador(contador -1); 
+  }
+  function ZeroNumber(){
+    setContador(contador == 0); 
+   }
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>Exemplo apostila 3</Text>
-      <TouchableOpacity style={styles.button} onPress={() => alert('oi')}>
-        <Text style={styles.textButton}>Hello World!!!</Text>
+
+     
+
+      <TouchableOpacity style={styles.button}
+       onPress={() => AddNumber()}>
+
+        <Text style={styles.textCounter}>Somar +1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}
+       onPress={() => AddNumber()}>
+
+        <Text style={styles.textCounter}>Somar +1</Text>
+      </TouchableOpacity>
+    
+   
+      <TouchableOpacity style={styles.button}
+       onPress={() => SubNumber()}>
+
+        <Text style={styles.textCounter}>Subtarir -1</Text>
+
+      </TouchableOpacity><TouchableOpacity style={styles.button}
+       onPress={() => ZeroNumber()}>
+
+        <Text style={styles.textCounter}>Zerar</Text>
       </TouchableOpacity>
 
     </View>
