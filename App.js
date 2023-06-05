637e967cc25d48daf526f974056b0920185b4583
AllanLima0609 <124743524+AllanLima0609@users.noreply.github.com>
2023-06-05 19:56:39 -0300

    05_06

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
 import Constants from 'expo-constants';
-import Apostila04 from './components/apostila_04';
+
+import Apostila04_ativ from './components/apostila_04_ativ';
 
 let corApp = '#000';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila04 />
+      <Apostila04_ativ />
       <StatusBar style={corApp === '#000' ? 'light' : 'dark'} />
     </View>
   );
